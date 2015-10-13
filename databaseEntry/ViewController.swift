a9814f0a6f21273a0e27890c19bc5041ca1893ef
tgilbert11 <tgilbert11@users.noreply.github.com>
2015-10-13 06:17:55 -0700

    updated dummy print

--- databaseEntry/ViewController.swift
+++ databaseEntry/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UICollectionViewDat
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello-git again twice from remote")
+        print("hello-git again twice from remote edited again")
 
         // Do any additional setup after loading the view, typically from a nib.
         testStruct()
